79ad5876731fa6d80e825a9155e60d0e6b59be14
tahidul1 <contact.tahidul@gmail.com>
2021-11-12 23:21:08 +0600

    update firbase config

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,21 +1,3 @@
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_API_KEY,
-//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_APP_ID,
-// };
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBzg4cZaiJm7t40SAyu8KHRQJygTFivLcE",
-//     authDomain: "pottery-7f291.firebaseapp.com",
-//     projectId: "pottery-7f291",
-//     storageBucket: "pottery-7f291.appspot.com",
-//     messagingSenderId: "819748423406",
-//     appId: "1:819748423406:web:e9a2894b451148d1e815ff"
-// };
-
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
