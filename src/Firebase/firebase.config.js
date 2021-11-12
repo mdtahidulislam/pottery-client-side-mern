c122ddcb30053c8214a63c8c2126ced2d878907c
tahidul1 <87003271+tahidul1@users.noreply.github.com>
2021-11-12 19:26:11 +0600

    Update firebase.config.js

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,1 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBzg4cZaiJm7t40SAyu8KHRQJygTFivLcE",
-    authDomain: "pottery-7f291.firebaseapp.com",
-    projectId: "pottery-7f291",
-    storageBucket: "pottery-7f291.appspot.com",
-    messagingSenderId: "819748423406",
-    appId: "1:819748423406:web:e9a2894b451148d1e815ff"
-};
 
-export default firebaseConfig;
